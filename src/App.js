77b4ada0d27fbc394ee63c07a39bd03956461f9b
HemaSudha <hemasudha.a@way2news.com>
2023-11-03 10:26:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react'
 function App() {
+  const [dark, setDark] = useState("")
+
+  const handleModes = () => {
+    setDark(!dark)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={dark ? 'lightContainer' : 'darkContainer'}>
+      <h1>click to Change Mode</h1>
+      <button onClick={handleModes}>{dark ? 'Light mode' : 'Dark Mode'}</button>
     </div>
   );
 }
